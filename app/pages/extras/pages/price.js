4975f7eb146db70ebf5d18feb0bb14c484db580c
ahmed rana <muhammadahmedrana1992@yahoo.com>
2019-09-24 04:59:26 -0700

    html compeleted

--- app/pages/extras/pages/price.js
+++ app/pages/extras/pages/price.js
@@ -6,7 +6,7 @@ var appPrice = {
             <div class="page-title">
                 <h2 class="text-md text-highlight">Price</h2><small class="text-muted">Service pricing table</small></div>
             <div class="flex"></div>
-            <div><a href="https://themeforest.net/item/basik-responsive-bootstrap-web-admin-template/23365964" class="btn btn-md text-muted"><span class="d-none d-sm-inline mx-1">Buy this Item</span> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></a></div>
+            
         </div>
     </div>
     <div class="page-content page-container" id="page-content">
--- app/pages/uiElements/charts/chartjs.js
+++ app/pages/uiElements/charts/chartjs.js
@@ -1,17 +1,215 @@
 var appChartsjs = {
     html: `
-    <div>
-        <div class="page-hero page-container" id="page-hero">
-            <div class="padding d-flex">
-                <div class="page-title">
-                    <h2 class="text-md text-highlight">Chartsjs</h2>
-                    <small class="text-muted">Start application in a blank page</small>
-                </div>
-            </div>
-        </div>
-        <div class="page-content page-container" id="page-content">
-        </div>
-    </div>
+    <div class="">
+	<div class="page-hero page-container" id="page-hero">
+		<div class="padding d-flex">
+			<div class="page-title">
+				<h2 class="text-md text-highlight">Chartjs</h2>
+				<small class="text-muted">Simple HTML5 Charts using the canvas tag</small>
+			</div>
+			<div class="flex"></div>
+			
+		</div>
+	</div>
+	<div class="page-content page-container" id="page-content">
+		<div class="padding">
+			<div class="row">
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Line chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-line" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Bar chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-bar" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Radar chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-radar" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Polar Area chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-polar" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Pie chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-pie" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Doughnut chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-doughnut" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Bubble chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-bubble" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Line line chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-line-line" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Bar line chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-line-bar" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Bar bar chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-bar-bar" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Bar bar chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-bar-bar-h" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="card">
+						<div class="card-header">Line line chart</div>
+						<div class="card-body" style="height: 240px">
+							<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
+								<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+								</div>
+								<div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
+									<div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+								</div>
+							</div>
+							<canvas id="chart-line-line-line" width="296" height="200" class="chartjs-render-monitor" style="display: block; width: 296px; height: 200px;"></canvas>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
     `,
     init: function() {
         document.getElementById('content').innerHTML = this.html;
